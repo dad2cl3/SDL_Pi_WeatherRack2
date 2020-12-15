927f2c17eeda5f8df174719d412426de7fb69832
dad2cl3 <jason@dad2cl3.com>
2020-12-14 20:59:41 -0500

    Added additional columns to t_weather_readings in order to support unique index on readings

--- pgsql/create-table-t_weather_readings.sql
+++ pgsql/create-table-t_weather_readings.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS weather.t_weather_readings (
     reading_id INT8 NOT NULL DEFAULT NEXTVAL('weather.seq_weather_reading_id'),
     reading_ts TIMESTAMP NOT NULL,
+    reading_device VARCHAR(100) NOT NULL,
+    reading_device_channel INT2,
     reading JSONB NOT NULL,
     CONSTRAINT pk_t_weather_readings_reading_id PRIMARY KEY (reading_id)
 );
@@ -8,3 +10,5 @@ CREATE TABLE IF NOT EXISTS weather.t_weather_readings (
 ALTER TABLE IF EXISTS weather.t_weather_readings OWNER TO "Jason";
 
 ALTER SEQUENCE IF EXISTS weather.seq_weather_reading_id OWNED BY weather.t_weather_readings.reading_id;
+
+CREATE UNIQUE INDEX unq_reading ON weather.t_weather_readings (reading_ts, reading_device, reading_device_channel);
